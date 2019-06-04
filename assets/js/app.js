de463fbbdda447da81334c761fae2825544225dd
Sho Tanaka <swallowtail.722.kyoto@gmail.com>
2019-06-04 22:48:11 +0900

    bootstrap導入

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,65 @@
 $(function() {
 
+    // 右ウインクの処理
+    let winkRight = "show";
+    $("#wink-right").on("click", function() {
+        
+        if (winkRight == "show") {
+            $("<p>").appendTo(".right-black").text("＜");
+            $(".right-black").css("backgroundColor", "#FFF");
+
+            winkRight = "hide";
+
+        } else if (winkRight == "hide") {
+            $(".right-black").children("p").remove();
+            $(".right-black").css("backgroundColor", "#000");
+
+            winkRight = "show";
+        };
+    });
+
+    // 左ウインクの処理
+    let winkLeft = "show";
+    $("#wink-left").on("click", function() {
+        
+        if (winkLeft == "show") {
+            $("<p>").appendTo(".left-black").text(">");
+            $(".left-black").css("backgroundColor", "#FFF");
+
+            winkLeft = "hide";
+
+        } else if (winkLeft == "hide") {
+            $(".left-black").children("p").remove();
+            $(".left-black").css("backgroundColor", "#000");
+
+            winkLeft = "show";
+        };
+    });
+
+    //rotateのプラグイン
+    $("#rotate").on("click", function() {
+        $("#korosuke").rotate({
+            angle:0,
+            animateTo:360,
+        });
+    });
+
+    //animate.cssのプラグイン
+    $("#fadeout").on("click", function() {
+        $("#korosuke").addClass("animated rollOut");
+    })
+
+    $("#fadein").on("click", function() {
+        $("#korosuke").addClass("animated rollLeft");
+    })
+
+    // ボタンをクリックしてマイクを表示
+    $("#sing").on("click", function() {
+        $(".sing").fadeIn(3000);
+    })
+
     //マイクをクリックした時の処理
-    $("#microphone").click(function() {
+    $("#microphone").on("click", function() {
 
         //headerの文字列を変更
         $("header h1").text("♪はじめてのチュウ♪");
@@ -10,7 +68,7 @@ $(function() {
         $("#audio").get(0).play();
 
         // アニメーションのクラスを追加
-        $("#korosuke").addClass("swing");
+        $("#swing").addClass("swing");
 
         //曲が終了した時の処理
         $("#audio").on("ended", function(){
